4518606452373b25971bd39cbd03f37497b8a835
Daryll <dosis@csumb.edu>
2023-04-22 00:08:46 -0700

    installed redux and created a store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
+    {/* <React.StrictMode> */}
     <App />
-  </React.StrictMode>,
-)
+    {/* </React.StrictMode> */}
+  </Provider>
+);
