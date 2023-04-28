1e4a5754b903f9d567abdfeac021c04f622670d4
Daryll <dosis@csumb.edu>
2023-04-27 17:57:27 -0700

    added concurrently script to run both client and server at once

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 
