174708c46079ea0bc691c4288e3357c37160131b
Daryll <dosis@csumb.edu>
2023-04-27 18:40:13 -0700

    initialized redux and set up store and reducer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react';
 import './App.css';
 
+import { useSelector } from 'react-redux';
+
 function App() {
+  const jobs = useSelector(state => state.jobs.jobs);
+  console.log(jobs);
   return (
     <>
       <p>app</p>
--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import JobReducer from '../reducers/JobReducer';
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    jobs: JobReducer
+  }
 });
