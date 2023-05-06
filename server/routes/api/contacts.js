ab5f2642ff01aef88711ff89302c629f8317d12b
Jonathan Harvey <jonathanharvey22@gmail.com>
2023-05-06 13:50:04 -0700

    testing branch updates

--- server/routes/api/contacts.js
+++ server/routes/api/contacts.js
@@ -1,6 +1,69 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', () => console.log('contacts get route'));
+router.get('/', async (req, res) => {
+    try{
+        const contacts = await sql`SELECT * FROM contacts`
+        if(!contacts){
+            return res.status(400).json({ msg: 'no contacts found' })
+        }
+        res.json(contacts)
+    }catch (err) {
+        console.error(err)
+        res.status(500).json({msg: 'server error'})
+    }
+});
+
+router.post('/', async (req, res) => {
+    try{
+        const {
+            user_id,
+            first_name,
+            last_name,
+            company,
+            city,
+            current_job_title,
+            phone,
+            email,
+            linkedin,
+            twitter,
+            instagram,
+            other_social,
+            personal_site,
+            fk_linked_job 
+        } = req;
+        const valuesArray = [
+            user_id,
+            first_name, 
+            last_name, 
+            company, 
+            city, 
+            current_job_title, 
+            phone, 
+            email, 
+            linkedin, 
+            twitter, 
+            instagram, 
+            other_social, 
+            personal_site,
+            fk_linked_job
+        ]
+        const query = `INSERT INTO contact(user_id, first_name, last_name, company, city, current_job_title, phone, email, linkedin, twitter, instagram, other_social, personal_site, fk_linked_job) VALUES(${valuesArray.join(", ")});`
+        const results = await sql`${query}`
+        res.status(200).json({contacts: results.rows})
+    }catch (err) {
+        console.error(err)
+        res.status(500).json({msg: 'server error'})
+    }
+})
+
+router.patch('/', async (req, res) => {
+    try{
+        
+    }catch (err) {
+        console.error(err)
+        res.status(500).json({msg: 'server error'})
+    }
+})
 
 module.exports = router;
