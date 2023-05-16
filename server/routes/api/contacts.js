b5f3d6e4d907a241f117af1f34f6d7b6cf61059f
Jonathan Harvey <jonathanharvey22@gmail.com>
2023-05-15 22:38:12 -0700

    edit - some comments and bits before adding pg-format so I can continue building api

--- server/routes/api/contacts.js
+++ server/routes/api/contacts.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try{
+        // when searching for contacts, how should contacts be searchable? 
+        // first_name, last_name, company, linked_job_opening
+        // Order by: timestamp
+        
+        // SELECT * FROM contacts WHERE user_id = ${req.user_id} AND 
         const contacts = await sql`SELECT * FROM contacts`
         if(!contacts){
             return res.status(400).json({ msg: 'no contacts found' })
@@ -16,54 +21,35 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try{
+        // validate inputs here - check that values were provided, and omit the rows that weren't from both
+        // the contact() parameter as well as the VALUES() parameters. This is a fix until the 'pg' library
+        // is used which parameterizes everything for us.
         const {
-            user_id,
-            first_name,
-            last_name,
-            company,
-            city,
-            current_job_title,
-            phone,
-            email,
-            linkedin,
-            twitter,
-            instagram,
-            other_social,
-            personal_site,
-            fk_linked_job 
-        } = req;
-        const valuesArray = [
-            user_id,
-            first_name, 
-            last_name, 
-            company, 
-            city, 
-            current_job_title, 
-            phone, 
-            email, 
-            linkedin, 
-            twitter, 
-            instagram, 
-            other_social, 
-            personal_site,
-            fk_linked_job
-        ]
-        const query = `INSERT INTO contact(user_id, first_name, last_name, company, city, current_job_title, phone, email, linkedin, twitter, instagram, other_social, personal_site, fk_linked_job) VALUES(${valuesArray.join(", ")});`
+            rows_names
+        } = req.names;
+        const {
+            rows_values
+        } = req.values
+        const query = `INSERT INTO contact() VALUES(${rows_values.join(", ")});`
         const results = await sql`${query}`
         res.status(200).json({contacts: results.rows})
     }catch (err) {
         console.error(err)
         res.status(500).json({msg: 'server error'})
     }
-})
+});
 
 router.patch('/', async (req, res) => {
     try{
-        
+        // Needs input validation and parsing for updating multiple rows at a time
+        const { updateWhat, updateFrom, updateTo } = req
+        const query = `UPDATE contact SET ${updateWhat} = ${updateTo} WHERE ${updateWhat} = ${updateFrom};`;
+        const response = sql`${query}`
+        res.status(200).json({contacts: results. rows})
     }catch (err) {
         console.error(err)
         res.status(500).json({msg: 'server error'})
     }
-})
+});
 
 module.exports = router;
