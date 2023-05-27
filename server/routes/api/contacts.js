3afdc711d887e10838bb237ee7c3a71b068a1a9c
Jonathan Harvey <jonathanharvey22@gmail.com>
2023-05-26 21:25:48 -0700

    updates to pg-format syntax and comments

--- server/routes/api/contacts.js
+++ server/routes/api/contacts.js
@@ -11,12 +11,16 @@ const format = require('pg-format');
 // @ACCESS Private
 router.get('/', async (req, res) => {
     try{
-        // This query is a straightforward match, which means if searching for 'joh', 'john' will not be in the results, and 
-        // neither will jo.
-        // Likely to just use LIKE, as in, first_name LIKE '<first_name>%', or LIKE '%<first_name>' for misspellings or similar names, etc
+        // This query is a straightforward match, which means if searching for 'joh', 'john' will not be 
+        // in the results, and neither will jo.
+        // Will later add LIKE, as in, first_name LIKE '<first_name>%', or LIKE '%<first_name>' for 
+        // misspellings or similar names, etc
+        const { first, last, user_id } = req.body
+        // Query string will always begin with:
         let queryStarter = 'SELECT * FROM %I WHERE user_id = %L'
-        if(req.query.first && req.query.last){
-            const query = format(queryStarter = queryStarter + ` AND first_name = %L  AND last_name = %L ORDER BY timestamp DESC;`, 'contact', req.query.user_id, req.query.first, req.query.last)
+        // But if both first name and last name were added to the search,
+        if(first && last){
+            const query = format(queryStarter = queryStarter + ` AND first_name = %L  AND last_name = %L ORDER BY timestamp DESC;`, 'contact', user_id, first, last)
             const client = new Client(config)
             client.connect()
             client.query(query, (err, response) => {
@@ -27,9 +31,9 @@ router.get('/', async (req, res) => {
                 res.status(200).json(response)
                 client.end()
             })
-        }else if(req.query.first){
+        }else if(first){
             queryStarter = queryStarter + ` AND first_name = %L`
-            const query = format(queryStarter + ` ORDER BY timestamp DESC;`, 'contact', req.query.user_id, req.query.first)
+            const query = format(queryStarter + ` ORDER BY timestamp DESC;`, 'contact', user_id, first)
             const client = new Client(config)
             client.connect()
             client.query(query, (err, response) => {
@@ -40,9 +44,9 @@ router.get('/', async (req, res) => {
                 res.status(200).json(response)
                 client.end()
             })
-        }else if(req.query.last){
+        }else if(last){
             queryStarter = queryStarter + ` AND last_name = %L`;
-            const query = format(queryStarter + ` ORDER BY timestamp DESC;`, 'contact', req.query.user_id, req.query.last)
+            const query = format(queryStarter + ` ORDER BY timestamp DESC;`, 'contact', user_id, last)
             const client = new Client(config)
             client.connect()
             client.query(query, (err, response) => {
@@ -67,12 +71,11 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     try{
         // Should have client side input checks, and server side input validation.
-        // Filter inputs for SQL special characters, keywords?
+        // Integrate express validator
         // an array of the identifiers
         // needs secure parameterization for when adding the user_id to the query
-        const names = JSON.parse(req.query.names);
-        const values = JSON.parse(req.query.values);
-        const query = format(`INSERT INTO %I(user_id, ${names.join(", ")}) VALUES(${req.query.user_id}, %L);`, 'contact', values)
+        const { names, values } = req.body;
+        const query = format(`INSERT INTO %I(user_id, ${names.join(", ")}) VALUES(${req.body.user_id}, %L);`, 'contact', values)
         const client = new Client(config)
         client.connect()
         client.query(query, (err, response) => {
@@ -94,15 +97,14 @@ router.post('/', async (req, res) => {
 // @ACCESS Private
 router.patch('/', async (req, res) => {
     try{
-        // request needs to send JSON.stringified arrays as parts of the body,
-        // names array = ["first_name", "last_name"], newValues array = ["new value", "new value"]
-        const updateWhat = JSON.parse(req.query.names);
-        const updateTo = JSON.parse(req.query.newValues);
-        // merge two arrays strings, together throwing in the formatting for SQL updates
+        // request sends JSON.stringified arrays as parts of the body
+        const { updateWhat, updateTo, user_id, id } = req.body
+        // merge two arrays strings, together throwing in the formatting for SQL updates for updating a type STRING
+        // => <row_name> = '<new_row_value>'
         const setUpdate = updateWhat.map((element, index, array) => {
             return `${element} = '${updateTo[index]}'`
         })
-        const query = `UPDATE contact SET ${setUpdate.join(", ")} WHERE user_id = ${req.query.user_id} AND id = ${req.query.id};`;
+        const query = `UPDATE contact SET ${setUpdate.join(", ")} WHERE user_id = ${user_id} AND id = ${id};`;
         const client = new Client(config)
         client.connect()
         client.query(query, (err, response) => {
@@ -124,7 +126,9 @@ router.patch('/', async (req, res) => {
 // @ACCESS Private
 router.delete('/', async (req, res) => {
     try{
-        const query = `DELETE FROM contact WHERE user_id = ${req.query.user_id} AND id = ${req.query.id};`;
+        // Are DELETE requests authenticated and protected from injection?
+        const { user_id, id } = req.body;
+        const query = `DELETE FROM contact WHERE user_id = ${user_id} AND id = ${id};`;
         const client = new Client(config)
         client.connect()
         client.query(query, (err, response) => {
