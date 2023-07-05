2b20decedefb10c82a47485704745cbbcdb68691
Daryll <dosis@csumb.edu>
2023-07-05 11:04:19 -0700

    save point for jobtracker

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const format = require('pg-format');
 const { Client, config } = require('../../config/db');
 
+////////////////////@@@@@@@@@@TODO@@@@@@@@@@@@@@@@@@@@@
+// 1. add webhooks for session created
+// 2. add webhooks for session end
+
 // @route     POST api/users/new
 // @desc      Create a new User
 // @access    Private
@@ -10,8 +14,8 @@ router.post('/new', async (req, res) => {
   const client = new Client(config);
   client.connect();
   const clerk_id = req.body.data.id;
-  console.log(req.body);
-  console.log(req.body.data.user_id);
+  // console.log(req.body);
+  // console.log(req.body.data.user_id);
 
   console.log('create user after creating clerk account is triggered');
 
@@ -32,6 +36,33 @@ router.post('/new', async (req, res) => {
       // console.log(response.rows[0]);
       // res.status(200).json(response.rows[0]);
 
+      client.end();
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+router.post('/auth', async (req, res) => {
+  console.log('auth in users route triggered');
+  const client = new Client(config);
+  client.connect();
+  // const clerk_id = req.body.data.id;
+  const clerk_id = req.body.data.user_id;
+  // console.log(req.body);
+
+  const query = format('SELECT * FROM users WHERE clerk_id = %L', clerk_id);
+
+  try {
+    client.query(query, (err, response) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ msg: 'query error' });
+      }
+
+      console.log(response.rows[0]);
+      res.status(200).json(response.rows[0]);
       client.end();
     });
   } catch (err) {
