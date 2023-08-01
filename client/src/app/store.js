ac8ba683c82f4f06e0a3dadaf005caae98a11dd5
jonathan harvey <jonathanharvey22@gmail.com>
2023-07-31 22:00:20 -0700

    Continuing development of redux state slice for contacts, ready for component html

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import BoardReducer from '../reducers/BoardReducer';
 import SelectedJobReducer from '../reducers/SelectedJobReducer';
+import TimelineReducer from '../reducers/TimelineReducer';
+import ContactReducer from '../reducers/ContactReducer';
 
 export const store = configureStore({
   reducer: {
     board: BoardReducer,
     selectedJob: SelectedJobReducer
+    timeline: TimelineReducer,
+    contact: ContactReducer,
   }
 });
--- client/src/components/pages/Contacts/ContactsPage.jsx
+++ client/src/components/pages/Contacts/ContactsPage.jsx
@@ -2,22 +2,40 @@ import React, { useState, useEffect } from 'react';
 
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { getAllContacts } from '../../../reducers/ContactReducer';
-import { useAuth } from '@clerk/clerk-react';
+import { getContacts } from '../../../reducers/ContactReducer';
+import { useAuth, useSession } from '@clerk/clerk-react';
 
 import styles from './ContactsPage.module.css';
 
 export default function ContactsPage() {
 
     const { userId } = useAuth();
-
+    const { session } = useSession();
     const dispatch = useDispatch();
-
     const contactResults = useSelector((state) => state.contactResults);
+    const { searchQuery, setSearchQuery } = useState({
+        first: "",
+        last: "",
+        type: "empty",
+        strValue: "",
+        token: {}
+    })
 
-    const searchQuery = useSelector((state) => state.searchQuery);
+    async function onSubmitHandler(e) {
+        e.preventDefault();
+        const searchQuerySubmit = {
+          first: searchQuery.first,
+          last: searchQuery.last,
+          type: searchQuery.type,
+          strValue: searchQuery.strValue,
+          token: await session.getToken()
+        };
+        dispatch(getContacts(user_id, searchQuerySubmit));
+      }
 
-
+    useEffect(() => {
+        dispatch(getContacts(userId, searchQuery));
+    }, [dispatch]);
 
     return (
         <div className={styles.page_wrapper}>
