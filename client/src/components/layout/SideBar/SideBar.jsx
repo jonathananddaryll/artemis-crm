0c6c107a446cd6f544ee01db392c1ae1600c935d
Jonathan Harvey <jonathanharvey22@gmail.com>
2023-09-14 21:56:14 -0700

    Fix: Capitalization of filename resulted in import error

---
+++ client/src/components/layout/SideBar/SideBar.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import styles from './SideBar.module.scss';
+import {
+  useClerk,
+  UserButton,
+  useAuth,
+  useUser,
+  SignOutButton
+} from '@clerk/clerk-react';
+
+import { NavLink } from 'react-router-dom';
+
+export default function SideBar() {
+  const { isLoaded } = useAuth();
+  const { user } = useUser();
+  const { signOut } = useClerk();
+
+  const menuItems = [
+    { index: 1, text: 'Contacts', icon: '/icons/phone.svg', link: '/contacts' },
+    {
+      index: 2,
+      text: 'Documents',
+      icon: '/icons/file.svg',
+      link: '/documents'
+    },
+    { index: 3, text: 'Boards', icon: '/icons/table.svg', link: '/boards' }
+  ];
+
+  return (
+    <div className={styles.navContainer}>
+      <div className={styles.navUpper}>
+        <div className={styles.navHeading}>
+          <div className={styles.navBrand}>
+            <img src='/icons/deer.png' className={styles.logoImage} />
+            <h2 className={styles.logoText}>Artemis</h2>
+          </div>
+        </div>
+        <div className={styles.navMenu}>
+          {menuItems.map(({ index, text, icon, link }) => (
+            <NavLink key={index} to={link} className={styles.navMenuLink}>
+              <div className={styles.menuItem}>
+                <img src={icon} alt='' srcSet='' />
+                <p>{text}</p>
+              </div>
+            </NavLink>
+          ))}
+        </div>
+        {/* // Demo Sign in Button put this in landing page later */}
+        {user === null && <DemoSignIn />}
+      </div>
+      {isLoaded && user !== null && (
+        <div className={styles.footer}>
+          <UserButton />
+          <div className={styles.footerUserInfo}>
+            <p className={styles.fullNameText}>{user.fullName}</p>
+            <p className={styles.emailText}>
+              {user.emailAddresses[0].emailAddress}
+            </p>
+          </div>
+          {/* <SignOutButton /> */}
+          <div className={styles.footerButton}>
+            <button className={styles.signoutButton} onClick={() => signOut()}>
+              <i className='bi bi-box-arrow-right'></i>
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
