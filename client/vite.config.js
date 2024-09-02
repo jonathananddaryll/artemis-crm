d298fad8ddada26c1bc84fee66673d5c3a6c100e
Daryll Osis <daryllosis27@gmail.com>
2024-09-02 13:04:22 -0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5432/api',
+        target: 'https://lionfish-app-i73eg.ondigitalocean.app/api',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
