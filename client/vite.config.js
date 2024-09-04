0e104f1b5f5e8e2d6f191b0b8e10a4d4fc10f990
Daryll Osis <daryllosis27@gmail.com>
2024-09-04 16:51:16 -0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5432/api/api',
+        target: 'http://localhost:5432/api',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
